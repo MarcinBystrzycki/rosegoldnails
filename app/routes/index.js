14eb6a19b3004e6b2e954f667d07e3d0e910b8b7
Marcin Bystrzycki <bystrzycki@programa.pl>
2020-01-17 16:45:02 +0100

    carousel

--- app/routes/index.js
+++ app/routes/index.js
@@ -1,11 +1,13 @@
 import Route from '@ember/routing/route';
 import RSVP from 'rsvp';
+import { inject as service } from '@ember/service';
 
 export default class IndexRoute extends Route {
+    @service store;
     model() {
         return RSVP.hash({
             configuration: this.store.modelFor('configuration'),
-            page: this.store.query('index', {}),
+            page: this.store.findRecord('index', 1),
         })
     }
 };
