5ad5d5551bf60054fd17ea0d534c64eb612a9eec
Marcin Bystrzycki <bystrzycki@programa.pl>
2020-01-22 17:02:48 +0100

    scroll down button

--- app/components/carousel.js
+++ app/components/carousel.js
@@ -1,4 +1,12 @@
 import Component from '@ember/component';
+import { action } from "@ember/object";
 
-export default Component.extend({
-});
+export default class Carousel extends Component {
+    showScrollButton = false;
+    @action
+    scrollDown(e) {
+        e.preventDefault();
+        const scrollAnchor = document.querySelector('[data-scroll-anchor]');
+        scrollAnchor.scrollIntoView({ behavior: 'smooth' });
+    }
+}
